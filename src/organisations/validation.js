b65521868ccefe915365713a89bd7559c706d3cc
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-08-19 12:34:19 +0200

    remove required status  of name

--- src/organisations/validation.js
+++ src/organisations/validation.js
@@ -1,35 +1,37 @@
-const Joi = require('joi');
+const Joi = require("joi");
 
 const ObjectId = Joi.string().regex(/^[0-9a-fA-F]{24}$/);
 const _id = ObjectId.required();
 const name = Joi.string();
 const location = Joi.object({
-  type: Joi.string().valid('Point'),
+  type: Joi.string().valid("Point"),
   coordinates: Joi.array().length(2).items(Joi.number()),
 }).allow(null);
-const description = Joi.string().allow('');
-const website = Joi.string().uri({ scheme: ['http', 'https'] }).allow('');
-const email = Joi.string().email().allow('');
-const telephone = Joi.string().allow('');
-const address = Joi.string().allow('');
-const zipcode = Joi.string().min(3).max(10).allow('');
-const city = Joi.string().allow('');
+const description = Joi.string().allow("");
+const website = Joi.string()
+  .uri({ scheme: ["http", "https"] })
+  .allow("");
+const email = Joi.string().email().allow("");
+const telephone = Joi.string().allow("");
+const address = Joi.string().allow("");
+const zipcode = Joi.string().min(3).max(10).allow("");
+const city = Joi.string().allow("");
 const tags = Joi.array();
 const venues = Joi.array();
 const limit = Joi.number();
 const skip = Joi.number();
-const types = Joi.array().items(Joi.string().valid(['organisation', 'venue']));
+const types = Joi.array().items(Joi.string().valid(["organisation", "venue"]));
 const sort = Joi.string();
-const order = Joi.string().valid(['ascend', 'descend']);
+const order = Joi.string().valid(["ascend", "descend"]);
 const fields = Joi.array();
 const published = Joi.boolean();
 const funded = Joi.boolean();
-const openingHours = Joi.string().allow('');
+const openingHours = Joi.string().allow("");
 const accessibility = Joi.object({
   wheelchair: Joi.object({
-    accessible: Joi.string().valid(['yes', 'no', 'limited', 'unknown']),
+    accessible: Joi.string().valid(["yes", "no", "limited", "unknown"]),
     toilets: Joi.bool(),
-    description: Joi.string().allow(''),
+    description: Joi.string().allow(""),
   }),
   blind: Joi.object({
     germanLanguage: Joi.bool(),
@@ -38,7 +40,7 @@ const accessibility = Joi.object({
     braille: Joi.bool(),
     guidance: Joi.bool(),
     audioguide: Joi.bool(),
-    description: Joi.string().allow(''),
+    description: Joi.string().allow(""),
   }),
   deaf: Joi.object({
     germanLanguage: Joi.bool(),
@@ -48,14 +50,14 @@ const accessibility = Joi.object({
     signLanguage: Joi.bool(),
     hearingAid: Joi.bool(),
     videoGuide: Joi.bool(),
-    description: Joi.string().allow(''),
+    description: Joi.string().allow(""),
   }),
 });
 const transportation = Joi.object({
-  tram: Joi.string().max(130).allow(''),
-  bus: Joi.string().max(130).allow(''),
-  subway: Joi.string().max(130).allow(''),
-  railway: Joi.string().max(130).allow(''),
+  tram: Joi.string().max(130).allow(""),
+  bus: Joi.string().max(130).allow(""),
+  subway: Joi.string().max(130).allow(""),
+  railway: Joi.string().max(130).allow(""),
 });
 
 const search = {
@@ -106,7 +108,7 @@ const findById = {
 
 const create = {
   payload: {
-    name: name.required(),
+    name,
     location,
     description,
     published,
@@ -175,7 +177,7 @@ const remove = {
 const relation = {
   params: {
     _id: ObjectId.required(),
-    relation: Joi.string().valid(['venues']).required(),
+    relation: Joi.string().valid(["venues"]).required(),
     relId: ObjectId.required(),
   },
 };
