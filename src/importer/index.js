57b4807419556e39643f92ca2da73cced6c3c558
fabianmoronzirfas <fabianmoronzirfas@protonmail.ch>
2020-08-19 12:17:14 +0200

    log more info on validation errors

--- src/importer/index.js
+++ src/importer/index.js
@@ -1,9 +1,9 @@
-const csv = require('csvtojson');
+const csv = require("csvtojson");
 
 const assignType = (type) => {
-  if (type === 'VENUE') return ['venue'];
-  if (type === 'ORG') return ['organisation'];
-  return ['organisation', 'venue'];
+  if (type === "VENUE") return ["venue"];
+  if (type === "ORG") return ["organisation"];
+  return ["organisation", "venue"];
 };
 
 const assignTags = (tag, tags) => {
@@ -20,32 +20,37 @@ const assignTags = (tag, tags) => {
 };
 
 const register = async (server, options) => {
-  const csvFile = options.csvFile || './data/daten.csv';
+  const csvFile = options.csvFile || "./data/daten.csv";
 
   const data = await csv().fromFile(csvFile);
   const tags = await server.plugins.tags.service.find();
 
-  data.map(entry => ({
-    name: entry.Institution,
-    address: entry.Adresse,
-    zipcode: entry.PLZ,
-    city: entry.Ort,
-    website: entry.Webseite.startsWith('http') ? entry.Webseite : `https://${entry.Webseite}`,
-    types: assignType(entry.Typ),
-    tags: assignTags(entry.Sparte, tags),
-  })).forEach(async (entry) => {
-    try {
-      await server.plugins.organisations.service.create(entry);
-    } catch (err) {
-      if (err.name !== 'ValidationError') {
-        console.log(err);
+  data
+    .map((entry) => ({
+      name: entry.Institution,
+      address: entry.Adresse,
+      zipcode: entry.PLZ,
+      city: entry.Ort,
+      website: entry.Webseite.startsWith("http")
+        ? entry.Webseite
+        : `https://${entry.Webseite}`,
+      types: assignType(entry.Typ),
+      tags: assignTags(entry.Sparte, tags),
+    }))
+    .forEach(async (entry, i) => {
+      try {
+        await server.plugins.organisations.service.create(entry);
+      } catch (err) {
+        if (err.name !== "ValidationError") {
+          console.log("entry", entry, "number", i);
+          console.log(err);
+        }
       }
-    }
-  });
+    });
 };
 
 exports.plugin = {
-  name: 'tsb-importer',
-  version: '0.0.1',
+  name: "tsb-importer",
+  version: "0.0.1",
   register,
 };
